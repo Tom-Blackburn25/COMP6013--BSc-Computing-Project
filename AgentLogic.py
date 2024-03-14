db8d5f2b331da51df931f548f71719f5aebba746
Tom-Blackburn25 <tom.blackburn2543@gmail.com>
2024-03-14 12:04:40 +0000

    added probabilities to nodes that were missing

--- AgentLogic.py
+++ AgentLogic.py
@@ -72,10 +72,13 @@ class Agentlogic:
             5: 2.816901408,  # Stables Exhibition
             6: 1.877934272,  # Stables Cafe
             7: 1.408450704,  # Churchill Exhibition
+            9: 3, #Clock Arch - MadeUp
+            10: 2.5, #Retail to CV - Madeup
             11: 4.460093897,  # Retail Toilet
             12: 1.408450704,  # Palace Inside 2
             14: 0,  # Churchill Male Toilet
             15: 0.234741784,  # Churchill Female Toilet
+            16: 5, # Formal Gardens - Madeup
             17: 10.79812207,  # West Courtyard
             18: 0.704225352,  # Stables Male toilet
             19: 2.34741784  # Stables Female toilet
@@ -88,10 +91,13 @@ class Agentlogic:
             5: 8.952899961,  # Stables Exhibition
             6: 2.296613468,  # Stables Cafe
             7: 7.940833009,  # Churchill Exhibition
+            9: 3, #Clock Arch - MadeUp
+            10: 2.5, #Retail to CV - Madeup
             11: 7.123394317,  # Retail Toilet
             12: 3.269754768,  # Palace Inside 2
             14: 0.350330868,  # Churchill Male Toilet
             15: 0.622810432,  # Churchill Female Toilet
+            16: 5, # Formal Gardens - Madeup
             17: 7.473725185,  # West Courtyard
             18: 0.233553912,  # Stables Male toilet
             19: 0.272479564  # Stables Female toilet
@@ -104,10 +110,13 @@ class Agentlogic:
             5: 7.285744367,  # Stables Exhibition
             6: 2.295220046,  # Stables Cafe
             7: 9.854706254,  # Churchill Exhibition
+            9: 3, #Clock Arch - MadeUp
+            10: 2.5, #Retail to CV - Madeup
             11: 6.780374816,  # Retail Toilet
             12: 12.00252685,  # Palace Inside 2
             14: 0.463255422,  # Churchill Male Toilet
             15: 0.758054327,  # Churchill Female Toilet
+            16: 5, # Formal Gardens - Madeup
             17: 7.369972626,  # West Courtyard
             18: 0.58959781,  # Stables Male toilet
             19: 1.052853232  # Stables Female toilet
@@ -120,10 +129,13 @@ class Agentlogic:
             5: 7.828434313,  # Stables Exhibition
             6: 2.984403119,  # Stables Cafe
             7: 16.15176965,  # Churchill Exhibition
+            9: 3, #Clock Arch - MadeUp
+            10: 2.5, #Retail to CV - Madeup
             11: 4.349130174,  # Retail Toilet
             12: 12.32753449,  # Palace Inside 2
             14: 1.484703059,  # Churchill Male Toilet
             15: 1.469706059,  # Churchill Female Toilet
+            16: 5, # Formal Gardens - Madeup
             17: 9.328134373,  # West Courtyard
             18: 0.419916017,  # Stables Male toilet
             19: 0.569886023  # Stables Female toilet
@@ -136,10 +148,13 @@ class Agentlogic:
             5: 4.803843074,  # Stables Exhibition
             6: 2.61008807,  # Stables Cafe
             7: 18.15852682,  # Churchill Exhibition
+            9: 3, #Clock Arch - MadeUp
+            10: 2.5, #Retail to CV - Madeup
             11: 4.883907126,  # Retail Toilet
             12: 11.52922338,  # Palace Inside 2
             14: 1.168935148,  # Churchill Male Toilet
             15: 1.024819856,  # Churchill Female Toilet
+            16: 5, # Formal Gardens - Madeup
             17: 8.710968775,  # West Courtyard
             18: 0.64051241,  # Stables Male toilet
             19: 1.393114492  # Stables Female toilet
@@ -152,10 +167,13 @@ class Agentlogic:
             5: 5.77462906,  # Stables Exhibition
             6: 3.515572784,  # Stables Cafe
             7: 17.52439513,  # Churchill Exhibition
+            9: 3, #Clock Arch - MadeUp
+            10: 2.5, #Retail to CV - Madeup
             11: 2.780376955,  # Retail Toilet
             12: 9.864991311,  # Palace Inside 2
             14: 1.042641358,  # Churchill Male Toilet
             15: 1.082742949,  # Churchill Female Toilet
+            16: 5, # Formal Gardens - Madeup
             17: 8.795615559,  # West Courtyard
             18: 0.735195829,  # Stables Male toilet
             19: 1.096110146  # Stables Female toilet
@@ -168,10 +186,13 @@ class Agentlogic:
             5: 8.648476145,  # Stables Exhibition
             6: 4.786979416,  # Stables Cafe
             7: 11.72809957,  # Churchill Exhibition
+            9: 3, #Clock Arch - MadeUp
+            10: 2.5, #Retail to CV - Madeup
             11: 3.207276209,  # Retail Toilet
             12: 8.871868518,  # Palace Inside 2
             14: 0.73400351,  # Churchill Male Toilet
             15: 0.510611138,  # Churchill Female Toilet
+            16: 5, # Formal Gardens - Madeup
             17: 9.350566459,  # West Courtyard
             18: 0.686133716,  # Stables Male toilet
             19: 1.180788256  # Stables Female toilet
@@ -184,10 +205,13 @@ class Agentlogic:
             5: 4.43101712,  # Stables Exhibition
             6: 2.970795569,  # Stables Cafe
             7: 15.25679758,  # Churchill Exhibition
+            9: 3, #Clock Arch - MadeUp
+            10: 2.5, #Retail to CV - Madeup
             11: 6.218529708,  # Retail Toilet
             12: 8.056394763,  # Palace Inside 2
             14: 0.83081571,  # Churchill Male Toilet
             15: 0.704934542,  # Churchill Female Toilet
+            16: 5, # Formal Gardens - Madeup
             17: 8.358509567,  # West Courtyard
             18: 0.730110775,  # Stables Male toilet
             19: 0.27693857  # Stables Female toilet
@@ -200,10 +224,13 @@ class Agentlogic:
             5: 0,  # Stables Exhibition
             6: 13.46153846,  # Stables Cafe
             7: 0,  # Churchill Exhibition
+            9: 3, #Clock Arch - MadeUp
+            10: 2.5, #Retail to CV - Madeup
             11: 3.365384615,  # Retail Toilet
             12: 17.30769231,  # Palace Inside 2
             14: 0,  # Churchill Male Toilet
             15: 0.961538462,  # Churchill Female Toilet
+            16: 5, # Formal Gardens - Madeup
             17: 25.96153846,  # West Courtyard
             18: 1.442307692,  # Stables Male toilet
             19: 5.769230769  # Stables Female toilet
